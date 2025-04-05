7029ca6e85cfc23bc94d016d77397b460c378a25
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-04-06 04:55:25 +0530

    Created 1st card of Pokemon ,Fetching API

--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -4,51 +4,51 @@ import '../Styles/poke.css';
 const Pokemon = () => {
     const [pokemon, setPokemon] = useState(null);
 
-  const API='https://pokeapi.co/api/v2/pokemon/squirtle';
+    const API = 'https://pokeapi.co/api/v2/pokemon/squirtle';
 
-  const fetchPokemon=()=>{
-    fetch(API)
-    .then((res)=>res.json())
-    .then((data)=>{
-      setPokemon(data);
-        })
-        .catch((error)=>console.log(error));
-  }
+    const fetchPokemon = () => {
+        fetch(API)
+            .then((res) => res.json())
+            .then((data) => {
+                setPokemon(data);
+            })
+            .catch((error) => console.log(error));
+    }
 
-  useEffect(()=>{
-    fetchPokemon();
-  },[]);
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
 
-  console.log(pokemon);
+    console.log(pokemon);
 
-  if(!pokemon){
-    return <div>
-        <h1>Loading...</h1>
-    </div>
-  }
+    if (!pokemon) {
+        return <div>
+            <h1>Loading...</h1>
+        </div>
+    }
 
-if(pokemon){
-  return (
-    <section className='container'>
-      <header>
-        <h1>Lets Catch Pokemon</h1>
-      </header>
-      <ul className='card-demo'>
-        <li className='pokemon-card'>
-          <figure>
-            <img 
-            src={pokemon.sprites.other.dream_world.front_default}
-             alt={pokemon.name} 
-             className='pokemon-image'
-             />
+    if (pokemon) {
+        return (
+            <section className='container'>
+                <header>
+                    <h1>Lets Catch Pokemon</h1>
+                </header>
+                <ul className='card-demo'>
+                    <li className='pokemon-card'>
+                        <figure>
+                            <img
+                                src={pokemon.sprites.other.dream_world.front_default}
+                                alt={pokemon.name}
+                                className='pokemon-image'
+                            />
 
-          </figure>
-          <h1>{pokemon.name}</h1>
-        </li>
-      </ul>
-    </section>
-  )
-}
+                        </figure>
+                        <h1>{pokemon.name}</h1>
+                    </li>
+                </ul>
+            </section>
+        )
+    }
 }
 
 export default Pokemon;
